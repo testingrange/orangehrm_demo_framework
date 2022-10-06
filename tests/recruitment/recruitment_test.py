510c75443845ab5a28ba5a61fb852ab64dce677c
Stan Pash <testitatonce@gmail.com>
2022-10-06 13:55:40 -0400

    Added test cases to recruitment_test

--- tests/recruitment/recruitment_test.py
+++ tests/recruitment/recruitment_test.py
@@ -15,7 +15,7 @@ class RecruitmentTest(unittest.TestCase):
         self.rp = RecruitmentPage(self.driver)
         self.ts = TestStatus(self.driver)
 
-
+    @pytest.mark.run(order=1)
     def test_correct_required_fields_data_TCRP001(self, first_name="Stan", last_name="Smith", email="stan_smith@gmail.com"):
         self.rp.log_in_to_app()
         result1 = self.rp.verify_no_record_exists(first_name, last_name)
@@ -26,6 +26,7 @@ class RecruitmentTest(unittest.TestCase):
         result3 = self.rp.verify_record_exists(first_name, last_name)
         self.ts.mark_final("Successfully add new candidate", result3, "New candidate record is present")
 
+    @pytest.mark.run(order=2)
     def test_correct_required_fields_data_TCRP00601(self, first_name="Adam", last_name="Jonson", email="apjonson@gmail.com", middle_name="Peter", contact_number="(999) 123-45-67", vacancy_name="Associate IT Manager", keywords="associate, it, manager", date="2022-09-01", notes="Test test test", consent="v"):
         result1 = self.rp.verify_no_record_exists(first_name, last_name, middle_name, vacancy_name, date)
         self.ts.mark(result1, "Candidate record is absent before the test")
@@ -35,6 +36,7 @@ class RecruitmentTest(unittest.TestCase):
         result3 = self.rp.verify_record_exists(first_name, last_name, middle_name, vacancy_name, date)
         self.ts.mark_final("Successfully add new candidate", result3, "New candidate record is present")
 
+    @pytest.mark.run(order=3)
     def test_correct_required_fields_data_TCRP00602(self, first_name="Steven", last_name="Jonson", email="sjonson@gmail.com", middle_name="Jeremy", contact_number="(999) 123-45-67", vacancy_name="Senior QA Lead", keywords="associate, it, manager", date="2022-09-01", notes="Test test test", consent="v"):
         result1 = self.rp.verify_no_record_exists(first_name, last_name, middle_name, vacancy_name, date)
         self.ts.mark(result1, "Candidate record is absent before the test")
@@ -44,6 +46,7 @@ class RecruitmentTest(unittest.TestCase):
         result3 = self.rp.verify_record_exists(first_name, last_name, middle_name, vacancy_name, date)
         self.ts.mark_final("Successfully add new candidate", result3, "New candidate record is present")
 
+    @pytest.mark.run(order=4)
     def test_correct_required_fields_data_TCRP00603(self, first_name="Adam", last_name="Peterson", email="peterson@gmail.com", middle_name="James", contact_number="(999) 234-54-76", vacancy_name="Software Engineer", keywords="associate, it, manager", date="2022-09-01", notes="Test test test", consent="v"):
         result1 = self.rp.verify_no_record_exists(first_name, last_name, middle_name, vacancy_name, date)
         self.ts.mark(result1, "Candidate record is absent before the test")
@@ -53,9 +56,43 @@ class RecruitmentTest(unittest.TestCase):
         result3 = self.rp.verify_record_exists(first_name, last_name, middle_name, vacancy_name, date)
         self.ts.mark_final("Successfully add new candidate", result3, "New candidate record is present")
 
-    def test_successful_deleting_existing_record_TC611(self, first_name="Stan", last_name="Smith", email="stan_smith@gmail.com"):
+    def test_successful_deleting_existing_record_TC00611(self, first_name="Stan", last_name="Smith", email="stan_smith@gmail.com"):
         result1 = self.rp.verify_record_exists(first_name, last_name)
         self.ts.mark(result1, "Candidate record is present")
         self.rp.delete_existing_record(first_name, last_name)
         result2 = self.rp.verify_no_record_exists(first_name, last_name)
+        self.ts.mark_final("Successfully delete existing record", result2, "Candidate record is absent")
+
+    def test_correct_required_fields_data_TCRP006121(self, first_name="Adam", last_name="Jonson",
+                                                    email="apjonson@gmail.com", middle_name="Peter",
+                                                    contact_number="(999) 123-45-67",
+                                                    vacancy_name="Associate IT Manager",
+                                                    keywords="associate, it, manager", date="2022-09-01",
+                                                    notes="Test test test", consent="v"):
+        result1 = self.rp.verify_record_exists(first_name, last_name, middle_name, vacancy_name, date)
+        self.ts.mark(result1, "Candidate record is present")
+        self.rp.delete_existing_record(first_name, last_name, middle_name, vacancy_name, date)
+        result2 = self.rp.verify_no_record_exists(first_name, last_name, middle_name, vacancy_name, date)
+        self.ts.mark_final("Successfully delete existing record", result2, "Candidate record is absent")
+
+    def test_correct_required_fields_data_TCRP006122(self, first_name="Steven", last_name="Jonson",
+                                                email="sjonson@gmail.com", middle_name="Jeremy",
+                                                contact_number="(999) 123-45-67", vacancy_name="Senior QA Lead",
+                                                keywords="associate, it, manager", date="2022-09-01",
+                                                notes="Test test test", consent="v"):
+        result1 = self.rp.verify_record_exists(first_name, last_name, middle_name, vacancy_name, date)
+        self.ts.mark(result1, "Candidate record is present")
+        self.rp.delete_existing_record(first_name, last_name, middle_name, vacancy_name, date)
+        result2 = self.rp.verify_no_record_exists(first_name, last_name, middle_name, vacancy_name, date)
+        self.ts.mark_final("Successfully delete existing record", result2, "Candidate record is absent")
+
+    def test_correct_required_fields_data_TCRP006123(self, first_name="Adam", last_name="Peterson",
+                                                    email="peterson@gmail.com", middle_name="James",
+                                                    contact_number="(999) 234-54-76", vacancy_name="Software Engineer",
+                                                    keywords="associate, it, manager", date="2022-09-01",
+                                                    notes="Test test test", consent="v"):
+        result1 = self.rp.verify_record_exists(first_name, last_name, middle_name, vacancy_name, date)
+        self.ts.mark(result1, "Candidate record is present")
+        self.rp.delete_existing_record(first_name, last_name, middle_name, vacancy_name, date)
+        result2 = self.rp.verify_no_record_exists(first_name, last_name, middle_name, vacancy_name, date)
         self.ts.mark_final("Successfully delete existing record", result2, "Candidate record is absent")
