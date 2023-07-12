369425e74bdc01cfd58287de8253adcc02fa7eb8
Stan Pash <testitatonce@gmail.com>
2023-07-12 15:30:42 -0400

    candidates_test.py was created and test_demo_suite.py was updated

--- tests/recruitment/candidates_test.py
+++ tests/recruitment/candidates_test.py
@@ -7,7 +7,7 @@ import unittest
 import pytest
 
 @pytest.mark.usefixtures("sessionSetUp")
-class RecruitmentTest(unittest.TestCase):
+class CandidatesTest(unittest.TestCase):
 
     @pytest.fixture(autouse=True)
     def classSetup(self, sessionSetUp):
@@ -15,7 +15,7 @@ class RecruitmentTest(unittest.TestCase):
         self.cp = CandidatesPage(self.driver)
         self.ts = TestStatus(self.driver)
 
-    @pytest.mark.run(order=1)
+    #@pytest.mark.run(order=1)
     def test_correct_required_fields_data_TCRP001(self, first_name="Stan", last_name="Smith", email="stan_smith@gmail.com"):
         self.cp.log_in_to_app()
         result1 = self.cp.verify_no_record_exists(first_name, last_name)
@@ -26,7 +26,7 @@ class RecruitmentTest(unittest.TestCase):
         result3 = self.cp.verify_record_exists(first_name, last_name)
         self.ts.mark_final("Successfully add new candidate", result3, "New candidate record is present")
 
-    @pytest.mark.run(order=2)
+    #@pytest.mark.run(order=2)
     def test_correct_required_fields_data_TCRP00601(self, first_name="Adam", last_name="Jonson", email="apjonson@gmail.com", middle_name="Peter", contact_number="(999) 123-45-67", vacancy_name="Associate IT Manager", keywords="associate, it, manager", date="2022-09-01", notes="Test test test", consent="v"):
         result1 = self.cp.verify_no_record_exists(first_name, last_name, middle_name, vacancy_name, date)
         self.ts.mark(result1, "Candidate record is absent before the test")
@@ -36,7 +36,7 @@ class RecruitmentTest(unittest.TestCase):
         result3 = self.cp.verify_record_exists(first_name, last_name, middle_name, vacancy_name, date)
         self.ts.mark_final("Successfully add new candidate", result3, "New candidate record is present")
 
-    @pytest.mark.run(order=3)
+    #@pytest.mark.run(order=3)
     def test_correct_required_fields_data_TCRP00602(self, first_name="Steven", last_name="Jonson", email="sjonson@gmail.com", middle_name="Jeremy", contact_number="(999) 123-45-67", vacancy_name="Senior QA Lead", keywords="associate, it, manager", date="2022-09-01", notes="Test test test", consent="v"):
         result1 = self.cp.verify_no_record_exists(first_name, last_name, middle_name, vacancy_name, date)
         self.ts.mark(result1, "Candidate record is absent before the test")
@@ -46,7 +46,7 @@ class RecruitmentTest(unittest.TestCase):
         result3 = self.cp.verify_record_exists(first_name, last_name, middle_name, vacancy_name, date)
         self.ts.mark_final("Successfully add new candidate", result3, "New candidate record is present")
 
-    @pytest.mark.run(order=4)
+    #@pytest.mark.run(order=4)
     def test_correct_required_fields_data_TCRP00603(self, first_name="Adam", last_name="Peterson", email="peterson@gmail.com", middle_name="James", contact_number="(999) 234-54-76", vacancy_name="Software Engineer", keywords="associate, it, manager", date="2022-09-01", notes="Test test test", consent="v"):
         result1 = self.cp.verify_no_record_exists(first_name, last_name, middle_name, vacancy_name, date)
         self.ts.mark(result1, "Candidate record is absent before the test")
--- tests/test_suits/test_demo_suite.py
+++ tests/test_suits/test_demo_suite.py
@@ -1,15 +1,18 @@
 import unittest
 from tests.home.login_test import LoginTest
-from tests.recruitment.candidates_test import RecruitmentTest
+from tests.recruitment.candidates_test import CandidatesTest
 
 
 # Loading test cases from test classes to execute in the current test suite
 
 tc1 = unittest.TestLoader().loadTestsFromTestCase(LoginTest)
-tc2 = unittest.TestLoader().loadTestsFromTestCase(RecruitmentTest)
+tc2 = unittest.TestLoader().loadTestsFromTestCase(CandidatesTest)
 
 
 # Creating a test suite combining all test classes
 demoTest = unittest.TestSuite([tc1, tc2])
+#smokeTest = unittest.TestSuite([tc1])
 
+#
+# unittest.TextTestRunner(verbosity=2).run(tc1)
 unittest.TextTestRunner(verbosity=2).run(demoTest)
